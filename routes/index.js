e215517467bfdfbd3d54f4cfc78cf99d2746a8f9
Stephen Shepherd <slshephe@ucsd.edu>
2016-02-17 21:08:34 -0800

    Added Experiment Header

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,18 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-    projects["grid"] = false;
-  	res.render('index', projects);
+    var random_num = Math.random();
+    console.log(random_num);
+    
+    if (random_num >= 0) {
+      projects['grid'] = false;
+      res.render('index', projects);
+    } else {
+      res.redirect('/grid');
+    }
 };
 
 exports.viewGrid = function(req, res){
-    projects["grid"] = true;
+    projects['grid'] = true;
   	res.render('index', projects);
 };
